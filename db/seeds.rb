1ba7d399cf0495b9da64aad29c79c79fbc210b68
EC2 Default User <ec2-user@ip-172-31-21-172.ec2.internal>
2024-12-01 03:28:50 +0000

    seedsADD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 管理者ユーザーの作成
+admin_user = Admin.create!(
+  email: '000@000',
+  password: '000000',
+  password_confirmation: '000000',
+)
+
+# 一般ユーザーの作成
+user1 = User.create!(
+  email: 'user1@example.com',
+  password: 'password123',
+  password_confirmation: 'password123',
+  leader: false,  # 一般ユーザー
+  address: '大阪府大阪市',
+  transportation: 2,  # 交通手段（例：2 = バス）
+  status: true  # アカウントの状態
+)
+
+user2 = User.create!(
+  email: 'user2@example.com',
+  password: 'password123',
+  password_confirmation: 'password123',
+  leader: true,  # リーダー
+  address: '福岡県福岡市',
+  transportation: 3,  # 交通手段（例：3 = 自転車）
+  status: true  # アカウントの状態
+)
