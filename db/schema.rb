fe36818bab7bf35bd8b18a29132becc72d632516
EC2 Default User <ec2-user@ip-172-31-21-172.ec2.internal>
2024-12-04 02:11:29 +0000

    devise-correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_01_014503) do
+ActiveRecord::Schema.define(version: 2024_12_04_020759) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2024_12_01_014503) do
     t.string "address"
     t.integer "transportation", default: 0, null: false
     t.boolean "status", default: true, null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
