48a2dda51d482481804bfd9d27ed3e3bf281b271
EC2 Default User <ec2-user@ip-172-31-13-23.ap-northeast-1.compute.internal>
2024-12-09 02:49:09 +0000

    管理者機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :homes, only: [:index]
+    
+    # ユーザー管理（検索機能を追加）
+    resources :users, except: [:new, :create] do
+      collection do
+        get 'search'  # 検索機能を追加
+      end
+    end
+    
+    resources :posts, except: [:new, :create]  # 投稿管理
+  end
+
+  # その他のルーティング設定（顧客用やdeviseなど）
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :public do
     resources :users, only: [:index, :show, :edit, :update] do
-      # 退会処理のルートを追加
       post 'withdraw', on: :member
     end
     
@@ -14,15 +36,4 @@ Rails.application.routes.draw do
 
   root to: 'homes#top' # トップページをルートに設定
   get 'about', to: 'homes#about', as: 'about' # Aboutページのルートを設定
-
-  # 顧客用
-  devise_for :users, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
 end
