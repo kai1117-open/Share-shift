3c02d28e546fc3679f0b9886ca410a9ece288369
EC2 Default User <ec2-user@ip-172-31-13-23.ap-northeast-1.compute.internal>
2024-12-13 11:52:57 +0000

    チャットを送信した時のリダイレクトの修正

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,30 +1,22 @@
 class Public::ChatsController < ApplicationController
   # チャットルームの表示
   def show
-    # チャット相手のユーザーを取得
     @user = User.find(params[:id])
   
-    # 現在のユーザーが参加しているチャットルームの一覧を取得
     rooms = current_user.user_rooms.pluck(:room_id)
-  
-    # 相手ユーザーとの共有チャットルームが存在するか確認
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
   
     unless user_rooms.nil?
-      # 共有チャットルームが存在する場合、そのチャットルームを表示
       @room = user_rooms.room
     else
-      # 共有チャットルームが存在しない場合、新しいチャットルームを作成
       @room = Room.create
-      # チャットルームに現在のユーザーと相手ユーザーを追加
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
       UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
   
-    # チャットルームに関連付けられたメッセージを取得
     @chats = @room.chats
+    Rails.logger.debug "チャット一覧: #{@chats.inspect}" # 追加
   
-    # 新しいメッセージを作成するための空のChatオブジェクトを生成
     @chat = Chat.new(room_id: @room.id)
   end
   
@@ -34,7 +26,10 @@ class Public::ChatsController < ApplicationController
     
     if @chat.save
       # チャット送信後、元のチャットルームにリダイレクト
-      redirect_to public_chat_path(@chat.room.id)
+      @room = Room.find(@chat.room_id)
+      @user = @room.user_rooms.where.not(user_id: current_user.id).first.user
+
+      redirect_to public_chat_path(@user.id)
     else
       flash.now[:alert] = "メッセージを入力してください。"
     
@@ -43,8 +38,8 @@ class Public::ChatsController < ApplicationController
       @user = @room.user_rooms.where.not(user_id: current_user.id).first.user
       @chats = @room.chats
       @chat = Chat.new(room_id: @room.id)
-    
-      redirect_to public_chat_path(@chat.room.id)
+  
+      redirect_to public_chat_path(@user.id)
     end
   end
   
