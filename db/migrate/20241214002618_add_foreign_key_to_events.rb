b6b9049a280deb4f9141c5b7388ee2a98d0a5390
EC2 Default User <ec2-user@ip-172-31-13-23.ap-northeast-1.compute.internal>
2024-12-20 10:44:27 +0000

    マイグレーションファイルの外部キー修正

--- db/migrate/20241214002618_add_foreign_key_to_events.rb
+++ db/migrate/20241214002618_add_foreign_key_to_events.rb
@@ -1,5 +1,6 @@
 class AddForeignKeyToEvents < ActiveRecord::Migration[6.1]
   def change
+    change_column :events, :group_id, :bigint
     add_foreign_key :events, :groups
   end
 end
--- db/migrate/20241216000853_create_group_tags.rb
+++ db/migrate/20241216000853_create_group_tags.rb
@@ -1,10 +1,12 @@
 class CreateGroupTags < ActiveRecord::Migration[6.1]
   def change
     create_table :group_tags do |t|
-      t.integer :group_id
+      t.bigint :group_id  # 修正：bigintに変更
       t.string :tag_name, null: false
 
       t.timestamps
     end
+
+    add_foreign_key :group_tags, :groups  # 外部キー制約を追加
   end
 end
